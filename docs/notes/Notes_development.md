e2590f773a3fdb51d3a02ffb338a62a94fdd521f
Katherine Eaton <katherine.eaton@phac-aspc.gc.ca>
2022-04-08 15:00:22 -0500

    fix typo in csv path for sc2rf

--- docs/notes/Notes_development.md
+++ docs/notes/Notes_development.md
@@ -1,5 +1,7 @@
 # Development
 
 1. Add lineage `XS` to controls.
+1. Add lineage `XQ` to controls.
+    - Currently not being detected by sc2rf though.
 1. Exclude lineage `XR` because it has not public genomes.
     - `XR` decends from `XQ` in the UShER tree.
--- scripts/sc2rf.sh
+++ scripts/sc2rf.sh
@@ -54,19 +54,19 @@ echo "python3 sc2rf.py ${aligned} ${sc2rf_args[@]}" > ${outdir}/${prefix}.txt;
 python3 sc2rf.py ${aligned} ${sc2rf_args[@]} 1>> ${outdir}/${prefix}.txt 2> ${log};
 
 # Check if any recombinants were detected"
-if [[ -s ${prefix}.csv ]]; then
+if [[ -s ${outdir}/${prefix}.csv ]]; then
 
-# rename tabular columns
-# + placeholder until breakpoints/intermissions regions are reported
-csvtk rename -f "sample" -n "strain" ${outdir}/${prefix}.csv \
-  | csvtk rename -f "examples" -n "sc2rf_clades" \
-  | csvtk rename -f "regions" -n "sc2rf_clades_regions" \
-  | csvtk rename -f "intermissions" -n "sc2rf_intermissions" \
-  | csvtk rename -f "breakpoints" -n "sc2rf_breakpoints" \
-  | csvtk mutate2 -n "sc2rf_breakpoints_regions" -e '""' \
-  | csvtk mutate2 -n "sc2rf_intermissions_regions" -e '""' \
-  | csvtk csv2tab \
-  > ${outdir}/${prefix}.tsv
+  # rename tabular columns
+  # + placeholder until breakpoints/intermissions regions are reported
+  csvtk rename -f "sample" -n "strain" ${outdir}/${prefix}.csv \
+    | csvtk rename -f "examples" -n "sc2rf_clades" \
+    | csvtk rename -f "regions" -n "sc2rf_clades_regions" \
+    | csvtk rename -f "intermissions" -n "sc2rf_intermissions" \
+    | csvtk rename -f "breakpoints" -n "sc2rf_breakpoints" \
+    | csvtk mutate2 -n "sc2rf_breakpoints_regions" -e '""' \
+    | csvtk mutate2 -n "sc2rf_intermissions_regions" -e '""' \
+    | csvtk csv2tab \
+    > ${outdir}/${prefix}.tsv
 
 # If not, touch an empty tabular file
 else
