27bbff0a2882c2f1a7c8cb5d05b93df1bda686d3
Katherine Eaton <katherine.eaton@phac-aspc.gc.ca>
2022-05-03 17:23:07 -0500

    generate geo_resolutions from ncov defaults lat longs

---
+++ resources/geo_resolutions.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import click
+import pandas as pd
+import json
+import os
+
+
+@click.command()
+@click.option(
+    "--lat-longs",
+    help="ncov lat_longs.tsv",
+    required=False,
+    default="ncov/defaults/lat_longs.tsv",
+)
+def main(lat_longs):
+    """Create a JSON of geo_resolutions from ncov lat_longs.tsv."""
+
+    lat_longs_df = pd.read_csv(lat_longs, sep="\t", header=None)
+    lat_longs_df.columns = ["resolution", "name", "latitude", "longitude"]
+
+    resolutions = set(lat_longs_df["resolution"])
+
+    geo_resolutions = []
+
+    for res in resolutions:
+        demes_dict = {}
+
+        res_df = lat_longs_df[lat_longs_df["resolution"] == res]
+        for rec in res_df.iterrows():
+            name = rec[1]["name"]
+            demes_dict[name] = {}
+            demes_dict[name]["latitude"] = rec[1]["latitude"]
+            demes_dict[name]["longitude"] = rec[1]["longitude"]
+
+        res_dict = {"key": res, "demes": demes_dict}
+        geo_resolutions.append(res_dict)
+
+    geo_resolutions_json = json.dumps(geo_resolutions, indent=2)
+
+    # Write Output
+    out_path = os.path.join("resources", "geo_resolutions.json")
+    with open(out_path, "w") as outfile:
+        outfile.write(geo_resolutions_json)
+
+
+if __name__ == "__main__":
+    main()
