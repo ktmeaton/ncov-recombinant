e77a3c6319c8c61bf4aaf1d52844cc7ae3e9ab7e
Katherine Eaton <katherine.eaton@phac-aspc.gc.ca>
2022-06-17 16:26:47 -0500

    script: implement low memory mode within usher_metadata script

--- scripts/usher_metadata.sh
+++ scripts/usher_metadata.sh
@@ -5,18 +5,47 @@
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    --base-input)
-      base_input=$2
+    --base-metadata)
+      base_metadata=$2
       shift # past argument
       shift # past value
       ;;
-    --build)
-      build=$2
+    --nextclade)
+      nextclade=$2
       shift # past argument
       shift # past value
+      ;;
+    --sc2rf)
+      sc2rf=$2
+      shift # past argument
+      shift # past value
+      ;;
+    --clades)
+      clades=$2
+      shift # past argument
+      shift # past value
+      ;;
+    --placement-stats)
+      placement_stats=$2
+      shift # past argument
+      shift # past value
+      ;;
+    --issue-to-lineage)
+      issue_to_lineage=$2
+      shift # past argument
+      shift # past value
+      ;;
+    --low-memory)
+      low_memory="true"
+      shift # past argument
       ;;
     --extra-cols)
       extra_cols=$2
+      shift # past argument
+      shift # past value
+      ;;
+    --output)
+      output=$2
       shift # past argument
       shift # past value
       ;;
@@ -27,29 +56,39 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-base_metadata="data/${base_input}/metadata.tsv"
-nextclade="results/${build}/nextclade.metadata.tsv"
-sc2rf="results/${build}/sc2rf.recombinants.tsv"
-usher_clades="results/${build}/usher.clades.tsv"
-usher_placement="results/${build}/usher.placement_stats.tsv"
-issue_to_lineage="resources/issue_to_lineage.tsv"
-
 default_cols="strain,date,country"
 extract_cols="clade,usher_clade,Nextclade_pango,usher_pango_lineage,sc2rf_parents,sc2rf_regions,sc2rf_breakpoints,usher_num_best"
 rename_cols="Nextstrain_clade,Nextstrain_clade_usher,pangolin_lineage,pango_lineage_usher,parents,parents_regions,breakpoints,usher_placements"
 rename_cols_final="clade_nextclade,clade_usher,lineage_nextclade,lineage_usher,parents,parents_regions,breakpoints,usher_placements"
 
 # Hack to fix commas if extra_cols is empty
+
 if [[ $extra_cols ]]; then
   extra_cols=",${extra_cols},"
 else
   extra_cols=","
 fi
 
-csvtk merge -t -f "strain" ${nextclade} ${sc2rf} ${usher_clades} ${usher_placement} \
-  | csvtk cut -t -f "${default_cols}${extra_cols}${extract_cols}" \
-  | csvtk rename -t -f "${extract_cols}" -n "${rename_cols}" \
-  | csvtk concat -t -u "?" - ${base_metadata} \
-  | csvtk cut -t -f "${default_cols}${extra_cols}${rename_cols}" \
-  | csvtk rename -t -f "${rename_cols}" -n "${rename_cols_final}" \
-  | csvtk replace -t -f "lineage_usher" -p "proposed([0-9]+)" -k ${issue_to_lineage} -r "{kv}"
+echo $low_memory
+# Low memory, don't use usher metadata
+if [[ "$low_memory" == "true" ]]; then
+
+  csvtk merge -t -f "strain" ${nextclade} ${sc2rf} ${clades} ${placement_stats} \
+    | csvtk cut -t -f "${default_cols}${extra_cols}${extract_cols}" \
+    | csvtk rename -t -f "${extract_cols}" -n "${rename_cols}" \
+    | csvtk cut -t -f "${default_cols}${extra_cols}${rename_cols}" \
+    | csvtk rename -t -f "${rename_cols}" -n "${rename_cols_final}" \
+    | csvtk replace -t -f "lineage_usher" -p "proposed([0-9]+)" -k ${issue_to_lineage} -r "{kv}" \
+    > $output;
+
+# Merge mode
+else
+  csvtk merge -t -f "strain" ${nextclade} ${sc2rf} ${clades} ${placement_stats} \
+    | csvtk cut -t -f "${default_cols}${extra_cols}${extract_cols}" \
+    | csvtk rename -t -f "${extract_cols}" -n "${rename_cols}" \
+    | csvtk concat -t -u "?" - ${base_metadata} \
+    | csvtk cut -t -f "${default_cols}${extra_cols}${rename_cols}" \
+    | csvtk rename -t -f "${rename_cols}" -n "${rename_cols_final}" \
+    | csvtk replace -t -f "lineage_usher" -p "proposed([0-9]+)" -k ${issue_to_lineage} -r "{kv}" \
+    > $output;
+fi
